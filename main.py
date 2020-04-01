d6f22a58bfcc24a187c8445dcbdc8016b849c1a8
Woynert <whayner.porras.2020@upb.edu.co>
2020-03-31 22:56:23 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Variables
+
+a = float(input("Ingrese el numero a --> "))
+c = float(input("Ingrese el numero c --> "))
+
+print("\nEl producto entre estos numeros es de: ",(a*c))
+print("Ya demás el doble de a es de: ",(2*a))
+
