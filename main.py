d4869fa227c293bde1289868f50e59160ee1e6b9
Woynert <whayner.porras.2020@upb.edu.co>
2020-03-31 23:05:14 -0500

    Mi primer edit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #Variables
 
 a = float(input("Ingrese el numero a --> "))
+b = float(input("Ingrese el numero b --> "))
 c = float(input("Ingrese el numero c --> "))
+d = float(input("Ingrese el numero d --> "))
 
-print("\nEl producto entre estos numeros es de: ",(a*c))
+print("\nEl producto entre a y d es de: ",(a*c))
 print("Ya demás el doble de a es de: ",(2*a))
 
+print("El cuadrado de b es de: ",(b*b))
+print("La raiz de d es de: ",d**(1/2))
