e8ec190e77a6633074aaebf62139997fbb94a1e5
Woynert <whayner.porras.2020@upb.edu.co>
2020-04-19 15:51:57 -0500

    G. Función mejor ciudad

--- Lab11/informe11_A_WhaynerPorrasJoseBenvanides.py
+++ Lab11/informe11_A_WhaynerPorrasJoseBenvanides.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+
+citys = np.array(["Bucaramanga", "Floridablanca", "Girón", "Piedecuesta"])
+
 #generar arrays
 def generador(_min, _max):
     return(np.random.randint(_min, _max, size = 48).reshape(4, 12))
@@ -8,7 +11,6 @@ def generador(_min, _max):
 def imprimir(arr):
     
     #indicadores
-    citys = np.array(["Bucaramanga", "Floridablanca", "Girón", "Piedecuesta"])
     print("Ene | Feb | Mar | Abr | May | Jun | Jul | Ago | Sep | Oct | Nov | Dic\n")
     
     #datos
@@ -34,6 +36,22 @@ def calculador(arrA, arrB):
     arrR = arrA - arrB
     return arrR
     
+#
+def mejor_ciudad(arr):
+    
+    mejor = 0; #por defecto
+    mejorGan = 0;
+    
+    for i in range(0, 4):
+        gan = 0;
+        for j in range(0, 12):
+            gan += arr[i, j];
+        if gan > mejorGan:
+            mejorGan = gan
+            mejor = i;
+    print("La ciudad con mejores ganancias fué:\n", citys[mejor], "\nCon ganancias de:",str(mejorGan)+"M COP")
+            
+
 #crear valores
 ingresos = generador(100, 180)
 egresos = generador(60, 130)
@@ -41,7 +59,13 @@ egresos = generador(60, 130)
 #imprimir valores
 print("INGRESOS")
 imprimir(ingresos)
+
 print("EGRESOS")
 imprimir(egresos)
+
 print("GANANCIAS")
-imprimir(calculador(ingresos, egresos))
+ganancias = calculador(ingresos, egresos)
+imprimir(ganancias)
+
+#mejor ciudad
+mejor_ciudad(ganancias)
