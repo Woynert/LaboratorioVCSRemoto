9e35414afa176be82d46e7414ff5c3289544f60a
Woynert <whayner.porras.2020@upb.edu.co>
2020-04-19 12:23:21 -0500

    C. creación función Imprimir

--- Lab11/informe11_A_WhaynerPorrasJoseBenvanides.py
+++ Lab11/informe11_A_WhaynerPorrasJoseBenvanides.py
@@ -1,7 +1,20 @@
 import numpy as np
 
 def generador(_min, _max):
-    return(np.random.randint(_min, _max, size = 64).reshape(4, 16))
+    return(np.random.randint(_min, _max, size = 48).reshape(4, 12))
 
+def imprimir(arr):
+    
+    #indicadores
+    citys = np.array(["Bucaramanga", "Floridablanca", "Girón", "Piedecuesta"])
+    print("Ene | Feb | Mar | Abr | May | Jun | Jul | Ago | Sep | Oct | Nov | Dic\n")
+    
+    #datos
+    for i in range(0, 4):
+        txt = ""
+        for j in range(0, 12):
+            txt += str(arr[i,j])+"   "
+        print(txt + citys[i])
+    
 ingresos = generador(100, 180)
 egresos = generador(60, 130)
